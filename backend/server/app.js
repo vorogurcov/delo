5b7c2e7fd8669d52ef707b93a14d3a8ce0935b79
vorogurcov <vania.frol2010@yandex.ru>
2025-01-09 12:48:10 +0300

    Change path of env config to correctly link it from backend dir

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-dotenv.config({path: './config/.env'});
+dotenv.config({path: './server/config/.env'});
 
 import express from 'express';
 import cookieParser from 'cookie-parser';
